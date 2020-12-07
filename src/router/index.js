9b1ba010a46278aa9c362754d46dcff37712309b
Picaqiuqiu <384912921@qq.com>
2020-12-07 12:13:21 +0800

    hjc

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-<<<<<<< HEAD
 import Home from '../views/Home.vue'
 import Payment from '../views/Payment_System.vue'
 import Door from '../views/Door.vue'
@@ -22,7 +21,6 @@ const routes = [{
         component: Door
     },
 ]
-=======
 import Home from '../views/home/Home.vue'
 import Repair from '../views/repair/Repair.vue'
 import Payment from '../views/payment/Payment_System.vue'
@@ -46,7 +44,7 @@ const routes = [{
     name: 'Repair',
     component: Repair
 }]
->>>>>>> remotes/origin/master
+remotes / origin / master
 
 const router = new VueRouter({
     routes
