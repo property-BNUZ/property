b168cdcc6b93160b9881995f27ec61874f97f694
Alex-3102 <1175106117@qq.com>
2020-12-07 18:59:10 +0800

    首页图片更换

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-<<<<<<< HEAD
-import Home from '../views/Home.vue'
-import Payment from '../views/Payment_System.vue'
-import Index from '../views/Index.vue'
-
-=======
 import Home from '../views/home/Home.vue'
 import Repair from '../views/repair/Repair.vue'
 import Payment from '../views/payment/Payment_System.vue'
 import Door from '../views/door/Door.vue'
->>>>>>> fd21f42184d58485f877e286b4f5968942a997b7
 Vue.use(VueRouter)
 
 import {
@@ -28,17 +21,6 @@ const routes = [{
     path: '/Payment',
     name: 'Payment',
     component: Payment
-<<<<<<< HEAD
-  },
-  {
-    path: '/Index',
-    name: 'Index',
-    component: Index
-  },
-  
-]
-
-=======
 }, {
     path: '/Repair',
     name: 'Repair',
@@ -48,7 +30,6 @@ const routes = [{
     name: 'Door',
     component: Door
 }, ]
->>>>>>> fd21f42184d58485f877e286b4f5968942a997b7
 const router = new VueRouter({
     routes
 })
