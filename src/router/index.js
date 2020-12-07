49aeab8e3a2f59efa38fbc2352cbbdc7a66e9913
Picaqiuqiu <384912921@qq.com>
2020-12-07 12:39:51 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Payment from '../views/Payment_System.vue'
+import Home from '../views/home/Home.vue'
+import Payment from '../views/payment/Payment_System.vue'
 import Door from '../views/Door.vue'
 Vue.use(VueRouter)
 
@@ -21,10 +21,10 @@ const routes = [{
         component: Door
     },
 ]
+
 import Home from '../views/home/Home.vue'
 import Repair from '../views/repair/Repair.vue'
 import Payment from '../views/payment/Payment_System.vue'
-import Door from '../views/Door.vue'
 import {
     Lazyload
 } from 'vant';
@@ -44,13 +44,7 @@ const routes = [{
     path: '/Repair',
     name: 'Repair',
     component: Repair
-}, {
-    path: '/Door',
-    name: 'Door',
-    component: Door
 }]
-remotes / origin / master
-
 const router = new VueRouter({
     routes
 })
