de7055d70062d31e9a760ebf73e0f9e945e84390
Picaqiuqiu <384912921@qq.com>
2020-12-07 11:58:05 +0800

    hjc

--- src/router/index.js
+++ src/router/index.js
@@ -2,23 +2,28 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Payment from '../views/Payment_System.vue'
+import Door from '../views/Door.vue'
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/Payment',
-    name: 'Payment',
-    component: Payment
-  },
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/Payment',
+        name: 'Payment',
+        component: Payment
+    },
+    {
+        path: '/Door',
+        name: 'Door',
+        component: Door
+    },
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
       <h1>物业管理app</h1>
       <router-link :to="{ path: '/Payment' }"> 缴费</router-link>
+      <router-link :to="{ path: '/Door' }"> 门禁</router-link>
    </div>
 </template>
 
