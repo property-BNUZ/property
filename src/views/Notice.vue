0bfd921464637ad7953e1db4abebb1dfa58c0700
Kelly <1225330367@qq.com>
2020-12-12 21:19:34 +0800

    notice

--- src/views/Notice.vue
+++ src/views/Notice.vue
@@ -1,15 +1,25 @@
 <template>
-    <div>
-        <label-box />
-    </div>
+    <div>
+        <notice-header></notice-header>
+        <notice-box></notice-box>
+        <el-divider>暂无数据</el-divider>
+        <label-box />
+    </div>
 </template>
 
 <script>
-    export default {
-        components: {}
-    }
+    import NoticeHeader from '../components/common/Header';
+    import LabelBox from '../components/common/LabelBox';
+    import NoticeBox from '../components/notie/notice_box.vue';
+    export default {
+        components: {
+            NoticeHeader,
+            NoticeBox,
+            LabelBox,
+        },
+    }
 </script>
 
-<style scoped>
+<style scoped>
 
 </style>
