98ceff226c0fefe0b024c345216eeb74b296f57f
Picaqiuqiu <384912921@qq.com>
2020-12-14 10:36:33 +0800

    hjc

--- src/views/Suggestion.vue
+++ src/views/Suggestion.vue
@@ -10,6 +10,46 @@
         </template>
       </van-card>
     </van-swipe-cell>
+    <el-dialog title="提示" :visible.sync="show" width="90%">
+      <van-field v-model="name" type="name" label="用户名" />
+      <van-field
+          v-model="sms"
+          center
+          clearable
+          label="短信验证码"
+          placeholder="请输入短信验证码"
+        >
+      <template #button>
+        <van-button size="small" type="primary" color="#1989fa" @click="sold">发送验证码</van-button>
+      </template>
+    </van-field>
+    </el-dialog>
+    <!-- 意见提交 -->
+  <van-field
+  v-model="message"
+  rows="15"
+  autosize
+  
+  type="textarea"
+  maxlength="500"
+  placeholder="请输入留言"
+  show-word-limit
+/>
+<div style="margin: 16px; color: #1989fa;" color="#1989fa">
+    <van-button round block type="primary" native-type="submit">
+      匿名提交
+    </van-button>
+  </div>
+  <van-divider :style="{ color: '#1989fa', borderColor: '#1989fa', padding: '0 16px' }">
+      提交图片
+    </van-divider>
+  <van-uploader v-model="fileList" multiple style="margin-left: 13px" />
+  <!-- 历史记录 -->
+  <van-dialog v-model="showList" title="历史记录如下" show-cancel-button>
+      <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+        <van-cell v-for="item in list" :key="item" :title="item" />
+      </van-list>
+    </van-dialog>
   </div>
 </template>
 
@@ -24,6 +64,31 @@
       },
     },
     methods: {
+      onLoad() {
+        // 异步更新数据
+        // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+        setTimeout(() => {
+          if(this.listm.length == 0){
+              this.list.push('暂无历史记录');
+            }
+            else{
+              for (let i = 0; i < this.listm.length; i++) {
+                this.list.push(this.listm[i]);
+                }
+            }
+
+          // 加载状态结束
+          this.loading = false;
+
+          // 数据全部加载完成
+          this.finished = true;
+        }, 1000);
+      },
+
+      sold() {
+        this.show = false;
+        this.Toast('提示内容');
+      },
       handleClick(row, flg) {
         if (flg) {
           this.money = row.money * 100 * -1;
@@ -45,8 +110,21 @@
         time: 0,
         currentRate: 0,
         percentage: 0,
-      }
-    },
+        show: true,
+        fileList: [{
+            url: 'https://img.yzcdn.cn/vant/leaf.jpg'
+          },
+        ],
+        name: '',
+        sms: '',
+        showList: false,
+        listm: [1,1,1],
+        loading: false,
+        finished: false,
+        list: [],
+        message: '',
+    }
+  }
   }
 </script>
 
